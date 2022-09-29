a4698fbeea42b954e4fc7a5b773b42b3793804c2
wdhub <583161751@qq.com>
2022-09-29 12:00:59 +0200

    get vivino data
    #Not solved:
    # maybe we shouldn't limit country. Or data is not enough and it take too much time to extract
    # (274 samples,184 second).
    # The labels are unbalanced: 274 samples, 39 negative

--- wine/getData.py
+++ wine/getData.py
@@ -1,90 +1,136 @@
 # download data from vivino
 
-#testing...
+#Not solved:
+# maybe we shouldn't limit country. Or data is not enough and it take too much time to extract
+# (274 samples,184 second).
+# The labels are unbalanced: 274 samples, 39 negative
 
 import requests
 import pandas as pd
 
+# boundary of rating to identify positive/negative label
+labelPositive=4.3
+labelNegative=3.3
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0",
+}
+
 
 def get_wine_data(wine_id, year, page):
-    headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0",
-    }
-
     api_url = "https://www.vivino.com/api/wines/{id}/reviews?per_page=50&year={year}&page={page}"  # <-- increased the number of reviews to 9999
-
     data = requests.get(
         api_url.format(id=wine_id, year=year, page=page), headers=headers
-    ).json()
+    ).json()# id=152262411, year=2013
+
+    filteredReview=[]# without rating between 2.9-4.0
+    for review in data['reviews']:
+        # fetch user country
+        seo_name=review['user']['seo_name']
+        country=get_user_country(seo_name)
+        review['user']['country']=country
+        # give label
+        review['label']=-1 # no label
+        if review['rating']>labelPositive:
+            review['label']=1
+        elif review['rating']<labelNegative:
+            review['label'] = 0
+        if review['label']!=-1:
+            filteredReview.append(review)
+
+    data['reviews']=filteredReview
 
     return data
 
+# fetch user country: 'united-states','sweden', ...
+def get_user_country(seo_name):
+    api_url = "https://www.vivino.com/api/users/{seo_name}"
+    data = requests.get(
+        api_url.format(seo_name=seo_name), headers=headers
+    ).json()["user"]["address"]["country"]["seo_name"]
 
-r = requests.get(
-    "https://www.vivino.com/api/explore/explore",
-    params={
-        "country_code": "FR",
-        "country_codes[]": "pt",
-        "currency_code": "EUR",
-        "grape_filter": "varietal",
-        "min_rating": "1",
-        "order_by": "price",
-        "order": "asc",
-        "page": 1,
-        "price_range_max": "500",
-        "price_range_min": "0",
-        "wine_type_ids[]": "1",
-    },
-    headers={
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0"
-    },
-)
+    return data
 
-results = [
-    (
-        t["vintage"]["wine"]["winery"]["name"],
-        t["vintage"]["year"],
-        t["vintage"]["wine"]["id"],
-        f'{t["vintage"]["wine"]["name"]} {t["vintage"]["year"]}',
-        t["vintage"]["statistics"]["ratings_average"],
-        t["vintage"]["statistics"]["ratings_count"],
+# fetch all wines under requirement
+# later: make price boundary as variable, number of pages
+def searchWines():
+    pageIndex=1
+    wineList=[]
+
+    #wine results of the first 25*4 wines
+    while pageIndex<=4:
+        r = requests.get(
+            "https://www.vivino.com/api/explore/explore",
+            params={
+                "country_code": "SE",
+                "language": "en",
+                #"country_codes[]": ["pt", "es", "fr"],
+                "currency_code": "SEK",
+                #"grape_filter": "varietal",
+                #"min_rating": "1",
+                "order_by": "ratings_count",
+                "order": "desc",
+                "page": pageIndex,
+                #"price_range_max": "500",
+                "price_range_min": "250",
+                "wine_type_ids[]": ["1","2","3","4","7","24"],#,all wine types
+            },
+            headers=headers,
+        )
+        wineList+=r.json()["explore_vintage"]["matches"]
+        print("explored wines in page "+str(pageIndex))
+        pageIndex+=1
+
+    results = [
+        (
+            t["vintage"]["wine"]["winery"]["name"],
+            t["vintage"]["year"],
+            t["vintage"]["wine"]["id"],
+            f'{t["vintage"]["wine"]["name"]} {t["vintage"]["year"]}',
+            t["price"]["amount"],
+            t["vintage"]["statistics"]["ratings_count"],
+        )
+        for t in wineList
+    ]
+    dataframe = pd.DataFrame(
+        results,
+        columns=["Winery", "Year", "Wine ID", "Wine", "price", "num_ratings"],
     )
-    for t in r.json()["explore_vintage"]["matches"]
-]
-dataframe = pd.DataFrame(
-    results,
-    columns=["Winery", "Year", "Wine ID", "Wine", "Rating", "num_review"],
-)
+    return dataframe
 
+dataframe=searchWines()
 ratings = []
 for _, row in dataframe.iterrows():
     page = 1
-    while page<2:# while true
+    while True:# while true
         print(
             f'Getting info about wine {row["Wine ID"]}-{row["Year"]} Page {page}'
         )
-
+        # fetch data of each wine that has more than 2000 rates
+        # otherwise there are not enough negative reviews
         d = get_wine_data(row["Wine ID"], row["Year"], page)
 
         if not d["reviews"]:
             break
 
+
         for r in d["reviews"]:
             ratings.append(
                 [
-                    row["Year"],
-                    row["Wine ID"],
-                    r["rating"],
+                    row["Wine"],
+                    row["price"],
+                    r['user']['country'],
+                    r['language'],
                     r["note"],
-                    r["created_at"],
+                    r["label"],
                 ]
             )
 
         page += 1
 
 ratings = pd.DataFrame(
-    ratings, columns=["Year", "Wine ID", "User Rating", "Note", "CreatedAt"]
+    ratings, columns=["Wine", "price", "country", "language","Note", "label"]
 )
 
-df_out = ratings.merge(dataframe)
-df_out.to_csv("data.csv", index=False)
+#df_out = ratings.merge(dataframe)
+ratings.to_csv("data.csv", index=False)
