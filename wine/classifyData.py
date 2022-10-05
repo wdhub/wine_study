8726d2b23f847373b912797fc3e34a51cbeb7dce
wdhub <583161751@qq.com>
2022-10-05 11:48:00 +0200

    fix data_en.csv

--- wine/classifyData.py
+++ wine/classifyData.py
@@ -2,11 +2,12 @@
 # feature for now: most frequent words
 # inbalance methods: weighted svm, ensemble of Bayesian
 
-
+import utility
 
 
 # read data, sort
-
+fileName="data_en.csv" # "I:/HCI_KTH/big data/project/codes/wine_study/wine/data_en.csv"
+comments, labels=utility.readCSV(fileName, 3,4)
 
 
 # extract features from dictionary
