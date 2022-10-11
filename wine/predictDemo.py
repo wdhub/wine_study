42d7194664c36dd90801e0d556c9e242e437ebb8
wdhub <583161751@qq.com>
2022-10-11 10:55:23 +0200

    Update predictDemo.py

--- wine/predictDemo.py
+++ wine/predictDemo.py
@@ -12,16 +12,20 @@ dataName = "I:/HCI_KTH/big data/project/codes/wine_study/wine/freq_dict.pkl"
 with open(dataName, 'rb') as load_data:
     dictWords = pickle.load(load_data)
 
-# show dict
-sortedDict=sorted(dictWords.vocabulary_.items(), key=lambda item: item[1], reverse=True)
-sortedDict[2500-200:2500]
+# # show dict
+# sortedDict=sorted(dictWords.vocabulary_.items(), key=lambda item: item[1], reverse=True)
+# sortedDict[2500-200:2500]
 
-inputString='Deep ruby nose of raspberry and plum, slight oak silky smooth tannins with long finish hint of chocolate can likely cellar for 5+ years '
+#inputString='Deep ruby nose of raspberry and plum, slight oak silky smooth tannins with long finish hint of chocolate can likely cellar for 5+ years '
 inputString='Way too much spices and artificial earthy wood notes giving a weirdly bitter aftertaste, together with the high alcohol and low tannin profile just not balanced.'
+#inputString='I do not like it!'
 
 # process input
 cleanedCom = utility.cleanText([inputString])  # clean special symbols etc
 X = dictWords.transform(cleanedCom).toarray()
 
 # predict
-model.predict(X)
+if model.predict(X)[0]==1:
+    print("market will like it!")
+else:
+    print("market may not like it!")
